bcf13e71b1ba33d1b0e8b2e7862dfa4c9effbdb6
Qingshan0153 <2594966517@qq.com>
2022-10-08 21:39:26 +0800

    Initial commit

---
+++ admin-parent/admin-webui/src/test/java/com/crowd/CrowdTest.java
@@ -0,0 +1,30 @@
+package com.crowd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * @author Mendax
+ * @date 2022-10-08 21:02
+ **/
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring-persist-mybatis.xml"})
+public class CrowdTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void getConnectionTest() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        System.out.println("connection = " + connection);
+    }
+}
