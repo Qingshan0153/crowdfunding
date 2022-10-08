d4dc2953d15811bf79cd35fa335282f2509adf2c
Qingshan0153 <2594966517@qq.com>
2022-10-08 22:42:38 +0800

    environment commit01

--- admin-parent/admin-webui/src/test/java/com/crowd/CrowdTest.java
+++ admin-parent/admin-webui/src/test/java/com/crowd/CrowdTest.java
@@ -1,7 +1,12 @@
 package com.crowd;
 
+import com.crowd.mappers.AdminMapper;
+import com.crowd.pojo.Admin;
+import com.sun.org.slf4j.internal.LoggerFactory;
+import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -22,9 +27,43 @@ public class CrowdTest {
     @Autowired
     private DataSource dataSource;
 
+    @Autowired
+    private AdminMapper adminMapper;
+
     @Test
     public void getConnectionTest() throws SQLException {
         Connection connection = dataSource.getConnection();
         System.out.println("connection = " + connection);
     }
+
+    @Test
+    public void adminMapperTest(){
+        Admin admin = adminMapper.selectByPrimaryKey(1);
+        System.out.println(admin);
+    }
+
+    public void logTest(){
+        LoggerFactory factory = new LoggerFactory();
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
