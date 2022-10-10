8885900758231fe2294732ab5ba404eafca07d85
Qingshan0153 <2594966517@qq.com>
2022-10-10 20:30:08 +0800

    login commit01

--- common-util/src/main/java/com/crowd/constant/CrowdConstant.java
+++ common-util/src/main/java/com/crowd/constant/CrowdConstant.java
@@ -10,4 +10,5 @@ public class CrowdConstant {
     public static final String MESSAGE_LOGIN_FAILED = "抱歉！账号密码错误！请重新输入！";
     public static final String MESSAGE_LOGIN_ACCT_ALREADY_IN_USE = "抱歉！账号已经被使用！";
     public static final String MESSAGE_ACCESS_LOGIN_AFTER = "请登录后访问！";
+    public static final String MESSAGE_STRING_INVALIDATE = "字符串不合法！";
 }
--- common-util/src/main/java/com/crowd/util/CrowdUtil.java
+++ common-util/src/main/java/com/crowd/util/CrowdUtil.java
@@ -1,6 +1,10 @@
 package com.crowd.util;
 
+import com.crowd.constant.CrowdConstant;
+
 import javax.servlet.http.HttpServletRequest;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * @author Mendax
@@ -8,11 +12,31 @@ import javax.servlet.http.HttpServletRequest;
  **/
 public class CrowdUtil {
 
+
+    public static String md5(String source) {
+
+        // 1.判断 source 是否有效
+        if (source == null || source.length() == 0) {
+            // 2. 不是有效字符串抛出异常
+            throw new RuntimeException(CrowdConstant.MESSAGE_STRING_INVALIDATE);
+        }
+
+        // 3. MessageDigest 加密方法  algorithm:算法
+        String algorithm = "md5";
+        try {
+            MessageDigest.getInstance(algorithm);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
     /**
      * 判断当前请求是否为 ajax 请求
      *
      * @param request request请求对象
-     * @return true:是Ajax请求 false: 不是Ajax请求     
+     * @return true:是Ajax请求 false: 不是Ajax请求
      */
     public static boolean judgeRequestType(HttpServletRequest request) {
 
